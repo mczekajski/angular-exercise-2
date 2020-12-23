8e9042ae998cae958a1df6af3629f0db54272161
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-12-23 19:14:38 +0100

    Add divider to modules

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { ModalComponent } from './modal/modal.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [ModsHomeComponent, ModalComponent],
   imports: [
     CommonModule,
-    ModsRoutingModule
+    ModsRoutingModule,
+    SharedModule
   ]
 })
 export class ModsModule { }
