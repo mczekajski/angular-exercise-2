1e6b8d433794d6f441feeaf00601674a109fdb4c
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-12-24 10:22:21 +0100

    Add accordion component

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'The sky is blue, because it is like that!' },
+    { title: 'What does and orange taste like?', content: 'An orange tastes like an orange!' },
+    { title: 'What color is that cat?', content: 'That cat is orange color.' }
+  ];
 
   constructor() { }
 
